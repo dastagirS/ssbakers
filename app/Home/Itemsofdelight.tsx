1b984da5be478e09093660d9e05f959fc9a8a697
dastagirS <shroud464@gmail.com>
2023-03-08 00:13:04 +0530

    add items of delight conditional css

--- app/Home/Itemsofdelight.tsx
+++ app/Home/Itemsofdelight.tsx
@@ -1,24 +1,29 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../globals.css";
 
 export default function Itemsofdelight() {
   const [items, setItems] = useState("Cakes");
+
+
   return (
     <section className="Itemsofdelight-main">
       <h4 className="Itemsofdelight-title">Items Of Delight</h4>
       <div className="Itemsofdelight-select">
         <input
+          className={items === "Cakes" ? "selected" : "unselected"}
           type={"button"}
           value={"Cakes"}
           onClick={(e) => setItems((e.target as HTMLInputElement).value)}
         />
         <input
+          className={items ==="Dry Products" ? "selected" : "unselected"}
           type={"button"}
           value={"Dry Products"}
           onClick={(e) => setItems((e.target as HTMLInputElement).value)}
         />
         <input
+          className={items === "Ice Cream" ? "selected" : "unselected"}
           type={"button"}
           value={"Ice Cream"}
           onClick={(e) => setItems((e.target as HTMLInputElement).value)}
